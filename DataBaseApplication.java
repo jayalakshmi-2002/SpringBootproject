10659d4371b7401e39c23095e5314ab7ac54f59e
Jayalakshmi A <jayalakshmi1407a@gmail.com>
2025-08-07 12:17:06 +0530

    DataBaseApplication.java

---
+++ DataBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.Databasse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabasseApplication.class, args);
+	}
+
+}
